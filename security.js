cdc69a100fe6e869d674868854fa0edc32cd9b8a
Waqas0003 <110096715+Waqas0003@users.noreply.github.com>
2025-08-14 20:31:01 +0500

    Update security.js

--- security.js
+++ security.js
@@ -1,12 +1,1 @@
 
-// test-alert.js
-(function () {
-  if (window.__TG_ALERT_SHOWN__) return; // avoid double alerts if enqueued twice
-  window.__TG_ALERT_SHOWN__ = true;
-
-  // Wait for DOM ready so it reliably fires whether loaded in head or footer
-  window.addEventListener('DOMContentLoaded', function () {
-    alert('External script loaded from GitHub via jsDelivr ✅');
-    console.log('Test alert script ran.');
-  });
-})();
